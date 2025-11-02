767fc62def663c461c8976e765d2ca76c42658ef
lefv <luis@lefv.io>
2025-11-02 19:29:01 +0100

    Add missing scrapers module exports to fix import errors

--- src/politician_trading/scrapers/__init__.py
+++ src/politician_trading/scrapers/__init__.py
@@ -2,3 +2,25 @@
 Politician Trading Data Workflow
 Tracks publicly available trading information for US and EU politicians
 """
+
+from .scrapers import (
+    CongressTradingScraper,
+    EUParliamentScraper,
+    PoliticianMatcher,
+    QuiverQuantScraper,
+    run_california_workflow,
+    run_eu_member_states_workflow,
+    run_uk_parliament_workflow,
+    run_us_states_workflow,
+)
+
+__all__ = [
+    "CongressTradingScraper",
+    "EUParliamentScraper",
+    "PoliticianMatcher",
+    "QuiverQuantScraper",
+    "run_california_workflow",
+    "run_eu_member_states_workflow",
+    "run_uk_parliament_workflow",
+    "run_us_states_workflow",
+]
